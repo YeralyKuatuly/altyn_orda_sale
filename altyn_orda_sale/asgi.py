5ceb6eaf8728745f0ae5372c5aa54f7d59f00349
YeralyKuatuly <yeralykuatuly@gmail.com>
2024-12-30 14:14:21 +0500

    first commit

---
+++ altyn_orda_sale/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for altyn_orda_sale project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'altyn_orda_sale.settings')
+
+application = get_asgi_application()
